0b6b24f71611971b729522fb94ded6eb17449d21
Tianci He <tiancihe@outlook.com>
2020-01-31 14:40:48 +0800

    add zip source script

---
+++ scripts/zip-source.js
@@ -0,0 +1,13 @@
+const cp = require("child_process")
+
+const { version } = require("../package.json")
+
+try {
+    cp.execSync(
+        `git archive master --format zip --output web-ext-artifacts/bookmark-manager-${version}-source.zip`
+    )
+    console.log("zip source code succeeded")
+} catch (err) {
+    console.log("zip source code failed:")
+    console.log(err.message)
+}
